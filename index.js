4d4987bae549b0a8be694651e5e307b768819b1d
ejricciardi <88864184+ejricciardi@users.noreply.github.com>
2025-05-08 11:15:11 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const socks = require('socksv5');
+
+const server = socks.createServer((info, accept, deny) => {
+  accept();
+});
+
+server.listen(1080, '0.0.0.0', () => {
+  console.log('SOCKS5 proxy server listening on port 1080');
+});
+
+server.useAuth(socks.auth.None());
